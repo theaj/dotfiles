1841dcaa695092890f8ccaf2c73d03bb17a67d2a
AJ Ravi <ajravindiran@gmail.com>
2023-01-04 19:04:30 -0500

    Remove old .zshrc file if it exists

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ setup_terminal() {
 
     if [[ ! -d $HOME/.oh-my-zsh ]]; then
         echo "=> Installing oh-my-zsh"
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
         echo "=> Installing zsh-autosuggestions"
         git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -31,10 +31,12 @@ setup_terminal() {
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
         echo "=> Importing zshrc"
-        ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+        rm -rf $HOME/.zshrc
+        ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 
         echo "=> Importing powerlevel10k"
-        ln -s $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
+        rm -rf $HOME/.p10k.zsh
+        ln -s $HOME/dotfiles/.p10k.zsh $HOME/.p10k.zsh
     fi
 }
 
